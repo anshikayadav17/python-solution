d697730414eee1b8274c55f117f613b64da4b9dc
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-06 09:18:11 +0530

    Create Snakegame.py

---
+++ Snakegame.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((600, 400))
+running = True
+
+while running:
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+pygame.quit()
