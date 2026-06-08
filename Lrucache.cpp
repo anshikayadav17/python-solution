c3af6b5910f3287eec66cb16185ee429bca7777b
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-08 08:57:27 +0530

    Add LRUCache class implementation
    Implement LRU Cache using OrderedDict for efficient access.

---
+++ Lrucache.cpp
@@ -0,0 +1,25 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+cache = LRUCache(3)
+cache.put(1, "A")
+cache.put(2, "B")
+cache.put(3, "C")
+print(cache.get(2))
