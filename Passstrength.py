736bcb4633487f07914dc6e5d6a0b05fab99adce
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-08 08:56:47 +0530

    Create Passstrength.py

---
+++ Passstrength.py
@@ -0,0 +1,18 @@
+import re
+
+password = input("Password: ")
+
+score = 0
+
+if len(password) >= 8:
+    score += 1
+if re.search(r"[A-Z]", password):
+    score += 1
+if re.search(r"[a-z]", password):
+    score += 1
+if re.search(r"\d", password):
+    score += 1
+if re.search(r"[!@#$%^&*]", password):
+    score += 1
+
+print("Score:", score, "/5")
