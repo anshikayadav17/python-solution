2deb1de17ba8a96daf04b043b68ba4a32f450f74
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-08 08:55:41 +0530

    Add asynchronous fetch example using aiohttp

---
+++ Async.cpp
@@ -0,0 +1,13 @@
+import aiohttp
+import asyncio
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+async def main():
+    html = await fetch("https://python.org")
+    print(len(html))
+
+asyncio.run(main())
