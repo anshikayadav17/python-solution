f2718bb9704a604b8948c37b9e490876076e1e41
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-10 18:04:14 +0530

    Create Lrucache.py

---
+++ Lrucache.py
@@ -0,0 +1,24 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+cache = LRUCache(2)
+cache.put(1, 10)
+cache.put(2, 20)
+print(cache.get(1))
