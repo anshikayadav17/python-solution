3615502f8ffc5e4905bb9bc0aed3c3fa9e816d54
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-17 10:50:33 +0530

    Create Caesar.py

---
+++ Caesar.py
@@ -0,0 +1,13 @@
+def encrypt(text, shift):
+    result = ""
+
+    for ch in text:
+        if ch.isalpha():
+            base = ord('A') if ch.isupper() else ord('a')
+            result += chr((ord(ch) - base + shift) % 26 + base)
+        else:
+            result += ch
+
+    return result
+
+print(encrypt("Python", 3))
