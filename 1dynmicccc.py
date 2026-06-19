c1fda43ed86d7bc35e42142b74fb0f862c39aca4
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-19 11:48:38 +0530

    Add coin change dynamic programming algorithm
    Implement dynamic programming solution for coin change problem.

---
+++ 1dynmicccc.py
@@ -0,0 +1,12 @@
+coins = [1, 2, 5]
+
+amount = 11
+
+dp = [float("inf")] * (amount + 1)
+dp[0] = 0
+
+for coin in coins:
+    for i in range(coin, amount + 1):
+        dp[i] = min(dp[i], dp[i - coin] + 1)
+
+print(dp[amount])
