4452c3211d3bee5f43518081e733e888fdd8a126
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-21 11:52:49 +0530

    Implement function to validate balanced parentheses

---
+++ Balanceddd.py
@@ -0,0 +1,20 @@
+def valid(s):
+    stack = []
+
+    pairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for ch in s:
+        if ch in "([{":
+            stack.append(ch)
+
+        else:
+            if not stack or stack.pop() != pairs[ch]:
+                return False
+
+    return len(stack) == 0
+
+print(valid("{[()]}"))
