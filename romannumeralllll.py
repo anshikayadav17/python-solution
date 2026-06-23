ee32fe79016533fb3ad72463193f551e065df9a8
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-23 13:43:31 +0530

    Create romannumeralllll.py

---
+++ romannumeralllll.py
@@ -0,0 +1,26 @@
+values = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I")
+]
+
+number = 1987
+
+roman = ""
+
+for value, symbol in values:
+    while number >= value:
+        roman += symbol
+        number -= value
+
+print(roman)
