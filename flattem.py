bdc5ac4db943789164235f4413df90ba0478fe66
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-23 13:44:10 +0530

    Create flattem.py

---
+++ flattem.py
@@ -0,0 +1,12 @@
+def flatten(data):
+    result = []
+
+    for item in data:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+
+    return result
+
+print(flatten([1, [2, [3, 4]], 5]))
