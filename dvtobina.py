ee16c9eda47c9e46c5a0e9b09d88517aeec546f9
anshikayadav17 <anshikayadav2917@gmail.com>
2026-06-25 21:05:22 +0530

    Create dvtobina.py

---
+++ dvtobina.py
@@ -0,0 +1,13 @@
+def decimal_to_binary(n):
+    if n == 0:
+        return "0"
+
+    result = ""
+
+    while n > 0:
+        result = str(n % 2) + result
+        n //= 2
+
+    return result
+
+print(decimal_to_binary(45))
